1e0135051d65a7f12c7430bece78a153aeb4c81c
ivalynx <inkybasik@gmail.com>
2022-09-27 20:25:29 +0300

    + news stitched-freedom-framed-in-belarus-project

---
+++ pages/News/stitched-freedom-framed-in-belarus-project/index.vue
@@ -0,0 +1,42 @@
+<style src="../News.scss" lang="scss"></style>
+
+<template>
+  <div>
+    <main-menu/>
+    <main class="Content">
+      <article>
+        <h1 class="Title">
+          <span class="content">STITCHED FREEDOM: “FRAMED IN BELARUS” PROJECT</span>
+        </h1>
+        <div class="content">
+          <p class="b">SURVIVAL KIT 13 / PILS 23, RIGA / 1-2.10.2022</p>
+          <p class="b">Stitchit art group is going to lead a workshop in Riga.</p>
+          <p><span class="b">On the 1st and 2nd of October at 2 pm EET</span> you are invited to participate in a <strong class="b">stitching workshop "Stitched Freedom: ‘Framed in Belarus’ project"</strong> led by art group "Stitchit" – Rufina Bazlova and Sofia Tocar. The workshop is a part of the Survival Kit festival program.</p>
+          <p class="b">Address: “Hearing Voices Café”, Pils iela 23, Riga</p>
+          <p>The aim of the #FramedinBelarus project is to tell about each wrongfully convicted citizen of Belarus and create their stories using the traditional Belarusian technique of embroidery with red thread on a white background, and as a result to fix an important age of the Belarusian history in a (inter)national folk work. By participating in the workshop you will be able to join the on-going project and become a part of the art solidarity community. </p>
+          <p>The participants of the workshop will be taught the first steps in working with scheme and stitching. Don’t be afraid if you have no experience with cross stitching before.</p>
+          <p>The workshop is free of charge (with a valid entrance ticket to the exhibition) and will be held in English.</p>
+          <p>All materials will be provided.</p>
+          <p>Please, register in advance through the <a href="https://forms.gle/pGwSyS8EjhHgtrk59">APPLICATION FORM</a>.</p>
+          <p>More information about the Survival Kit program <a href="https://lcca.lv/en/survival-kit-13/programme/1-october--stitched-freedom---framed-in-belarus--project/">here</a>.</p>
+          <Slider/>
+        </div>
+      </article>
+    </main>
+    <main-footer/>
+  </div>
+</template>
+
+<script>
+  import MainFooter from '@/components/general/MainFooter'
+  import MainMenu from '@/components/general/MainMenu'
+  import Slider from '@/components/general/Slider'
+  
+  export default {
+    components: {
+      MainFooter,
+      MainMenu,
+      Slider
+    },
+  }
+  </script>
