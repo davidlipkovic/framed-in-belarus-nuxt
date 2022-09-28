e1894fd4f24b66d2f1807d9ee1642fb512590dfb
ivalynx <inkybasik@gmail.com>
2022-09-28 12:23:14 +0300

    Changed picture for news THE LITTLE BIRD

--- pages/News/the-little-bird-must-be-caught/index.vue
+++ pages/News/the-little-bird-must-be-caught/index.vue
@@ -4,7 +4,7 @@
   <div>
     <main-menu/>
     <main class="Content">
-      <article>
+      <article class="news">
         <h1 class="Title">
           <span class="content">THE LITTLE BIRD MUST BE CAUGHT</span>
         </h1>
@@ -13,8 +13,11 @@
           <p>#FramedinBelarus in Riga!</p>
           <p>The group exhibition "<strong class="b">The little bird must be caught</strong>" presents a part of the project #FramedinBelarus on the 13th edition of the international contemporary art festival <strong class="b">Survival Kit</strong>, which takes place inside an old bank building, in the heart of <strong class="b">Dome Square (Pils iela 23, Riga) from 3rd of September to the 16th of October</strong>. </p>
           <p>The international contemporary art festival Survival Kit is one of the largest contemporary art events in the Baltics, taking place in Rīga annually. It was started in 2009 as a reaction towards the global economic crisis, calling for a consideration of various survival strategies within the changeable world of today. Every year, the festival is dedicated to a new and significant social topic. Abandoned Riga buildings are often used to hold festival events in order to draw attention to their future potential for development.</p>
+          <p>Contemporary Art Festival Survival Kit 13 “The Little Bird must be Caught” curated by iLiana Fokianaki, in Riga, September 2022.</p>
+          <p>Photo credit: Kristīne Madjare / Latvian Centre for Contemporary Art</p>
           <p>Read more: <a href="https://lcca.lv/en/survival-kit/">lcca.lv</a> / <a href="https://www.facebook.com/survivalkit.lv">Facebook</a> / <a href="https://www.instagram.com/latviancentre4contemporaryart">Instagram</a></p>
-          <Slider/>
+          <img class="img" alt="Photo credit: Kristīne Madjare / Latvian Centre for Contemporary Art" src="../../../assets/media/img/the-little-bird-must-be-caught.jpg">
+          <!-- <Slider/> -->
         </div>
       </article>
     </main>
