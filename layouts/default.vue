019e06954dec741ec71d1481712bb62583b64613
David L <david.lipkovic@gmail.com>
2022-10-02 22:34:52 +0200

    adds default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <main-menu/>
+    <slot/>
+    <main-footer/>
+  </div>
+</template>
+
+<script>
+import MainFooter from '@/components/general/MainFooter'
+import MainMenu from '@/components/general/MainMenu'
+
+export default {
+  components: {
+    MainFooter,
+    MainMenu
+  },
+}
+</script>
