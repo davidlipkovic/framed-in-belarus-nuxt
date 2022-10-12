e72f56ddd11a8b72f4d5821d6c6bad24eb5f5b6a
ivalynx <inkybasik@gmail.com>
2022-10-12 19:23:21 +0300

    Cut newslead to height (by designer)

--- pages/News/index.vue
+++ pages/News/index.vue
@@ -2,20 +2,22 @@
 
 <template>
   <main class="Content">
-    <h1 class="Title">
-      <span class="content">
+    <div class="Title">
+      <h1 class="content">
         {{ $t('newsPageTitle') }}
-      </span>
-    </h1>
+      </h1>
+    </div>
     <div class="Article content">
       <div class="section news">
         <img class="img" alt="THE LITTLE BIRD MUST BE CAUGHT" src="../../assets/media/img/news/the-little-bird-must-be-caught/title.jpg"/>
         <h2 class="title">
           {{ $t('newsPageArticle1Title') }}
         </h2>
-        <p>
+        <div class="newslead">
+          <p>
           {{ $t('newsPageArticle1Perex') }}
         </p>
+        </div>
         <nuxt-link
           to="news/the-little-bird-must-be-caught"
           class="button"
@@ -28,12 +30,14 @@
         <h2 class="title long">
           {{ $t('newsPageArticle2Title') }}
         </h2>
-        <p>
-          {{ $t('newsPageArticle2Perex') }}
-        </p>
-        <p>
-          {{ $t('newsPage1') }}
-        </p>
+        <div class="newslead">
+          <p>
+            {{ $t('newsPageArticle2Perex') }}
+          </p>
+          <p>
+            {{ $t('newsPage1') }}
+          </p>
+        </div>
         <nuxt-link
           to="news/stitched-freedom-framed-in-belarus-project"
           class="button"
