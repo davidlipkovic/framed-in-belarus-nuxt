e817a09c7e20778e175fafd6fd5b72dcc0b387ba
ivalynx <inkybasik@gmail.com>
2022-10-27 14:14:11 +0300

    Forgot password Step0

---
+++ pages/ForgotPassword/Step0/index.vue
@@ -0,0 +1,43 @@
+<style src="../../../assets/style/form.scss" lang="scss" scoped></style>
+<style src="../../SignUp/SignUp.scss" lang="scss" scoped></style>
+
+<template>
+  <header class="Header_sign">
+    <div class="content">
+      <a href="/" class="logo-project">
+        <span class="firstSymbol">#</span><span class="text">
+          {{ $t('title') }}
+        </span>
+      </a>
+      <input type="checkbox" id="Lang" class="Lang-checker visually-hidden">
+      <div class="Lang">
+        <label for="Lang" role="button" aria-role="button"
+          class="Lang-button"
+          >
+          EN<img class="arrow" src="@/assets/media/img/arrow.svg">
+        </label>
+        <div class="Lang-select">
+          <a href="/ru" class="Lang-link">Russian</a>
+        </div>
+      </div>
+    </div>
+  </header>
+  <main class="Content">
+    <div class="Embroiderer">
+      <img src="../../../../assets/media/img/embroiderer.svg" alt="Embroiderer" class="Img">
+    </div>
+    <div class="content">
+      <h1 class="title">Forgot password?</h1>
+      <form>
+        <p><label for="email">Enter your email address to which we will send a password recovery code</label></p>
+        <input type="email" name="email" id="email" placeholder="E-mail">
+        <button class="button bg_black large">Send</button>
+      </form>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
