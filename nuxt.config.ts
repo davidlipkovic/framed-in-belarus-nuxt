7fb035d1ced8b89f6eeb58aff9b8734e65baeead
David L <david.lipkovic@gmail.com>
2022-11-01 21:58:02 +0100

    changes meta tags, adds favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,23 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    // server: {
-    //     port: process.env.NUXT_ENV_PORT
-    // },
-
-    head: {
-        title: 'Framed in Belarus',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Framed in Belarus' },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/atelier_animace_fav.png' }
-        ]
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'en'
+            },
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'Framed in Belarus',
+            meta: [
+                { name: 'description', content: 'Framed in Belarus' },
+                { name: 'format-detection', content: 'telephone=no' },
+                { name: 'robots', content: 'noindex' }
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: './assets/media/fav.svg' }
+            ]
+        }
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
