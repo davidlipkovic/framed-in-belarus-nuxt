368715b313f519deb9e9eff3c10cb92c6f938c92
ivalynx <inkybasik@gmail.com>
2022-11-07 14:57:22 +0200

    + ru translate by News Stitched Freedom

--- pages/News/stitched-freedom-framed-in-belarus-project/index.vue
+++ pages/News/stitched-freedom-framed-in-belarus-project/index.vue
@@ -5,7 +5,7 @@
     <article>
       <div class="Title">
         <div class="content">
-          <h1 class="content News">Stitched freedom: “Framed in&nbsp;Belarus” project</h1>
+          <h1 class="content News">{{ $t('newsPageArticle2Title') }}</h1>
           <nuxt-link
             to="/News"
             class="GoToNews">
@@ -15,15 +15,15 @@
       </div>
       <div class="News content">
         <p class="b">SURVIVAL KIT 13 / PILS 23, RIGA / 1-2.10.2022</p>
-        <p class="b">Stitchit art group is going to lead a workshop in Riga.</p>
-        <p><span class="b">On the 1st and 2nd of October at 2 pm EET</span> you are invited to participate in a <strong class="b">stitching workshop "Stitched Freedom: ‘Framed in Belarus’ project"</strong> led by art group "Stitchit" – Rufina Bazlova and Sofia Tocar. The workshop is a part of the Survival Kit festival program.</p>
-        <p class="b">Address: “Hearing Voices Café”, Pils iela 23, Riga</p>
-        <p>The aim of the #FramedinBelarus project is to tell about each wrongfully convicted citizen of Belarus and create their stories using the traditional Belarusian technique of embroidery with red thread on a white background, and as a result to fix an important age of the Belarusian history in a (inter)national folk work. By participating in the workshop you will be able to join the on-going project and become a part of the art solidarity community. </p>
-        <p>The participants of the workshop will be taught the first steps in working with scheme and stitching. Don’t be afraid if you have no experience with cross stitching before.</p>
-        <p>The workshop is free of charge (with a valid entrance ticket to the exhibition) and will be held in English.</p>
-        <p>All materials will be provided.</p>
-        <p>Please, register in advance through the <a href="https://forms.gle/pGwSyS8EjhHgtrk59">APPLICATION FORM</a>.</p>
-        <p>More information about the Survival Kit program <a href="https://lcca.lv/en/survival-kit-13/programme/1-october--stitched-freedom---framed-in-belarus--project/">here</a>.</p>
+        <p class="b">{{ $t('newsStitchedFreedom0') }}</p>
+        <p>{{ $t('newsStitchedFreedom1') }}<span class="b">{{ $t('newsStitchedFreedom2') }}</span>{{ $t('newsStitchedFreedom3') }}<strong class="b">{{ $t('newsStitchedFreedom4') }}</strong></p>
+        <p class="b">{{ $t('newsStitchedFreedom5') }}</p>
+        <p>{{ $t('newsStitchedFreedom6') }}</p>
+        <p>{{ $t('newsStitchedFreedom7') }}</p>
+        <p>{{ $t('newsStitchedFreedom8') }}</p>
+        <p>{{ $t('newsStitchedFreedom9') }}</p>
+        <p>{{ $t('newsStitchedFreedom10') }} <a href="https://forms.gle/pGwSyS8EjhHgtrk59">{{ $t('newsStitchedFreedom11') }}</a>.</p>
+        <p>{{ $t('newsStitchedFreedom12') }} <a href="https://lcca.lv/en/survival-kit-13/programme/1-october--stitched-freedom---framed-in-belarus--project/">{{ $t('newsStitchedFreedom13') }}</a>.</p>
       </div>
     </article>
   </main>
