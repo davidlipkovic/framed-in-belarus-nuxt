ea9ef36dddd9075d362d5fd328e71d8f7fcd6d1e
ivalynx <inkybasik@gmail.com>
2022-11-09 20:13:37 +0200

    + Settings

--- pages/UserCabinet/index.vue
+++ pages/UserCabinet/index.vue
@@ -22,10 +22,15 @@ const editModal = ref({
     <div class="content">
       <section class="userCard">
         <div class="userCard-header">
-          <img src="../../assets/media/img/Avatar.png" class="userCard-avatar" alt="Avatar">
+          <img src="../../assets/media/img/Avatar.svg" class="userCard-avatar" alt="Avatar">
+          <label for="uploadAvatar" class="upload"><input type="file" name="uploadAvatar" id="uploadAvatar" class="visually-hidden">Upload foto</label>
           <h2 class="userCard-title">Alexander Ivanow</h2>
         </div>
         <div class="userCard-body">
+          <p class="userCard-body-item">
+            <span class="b">Pseudonym::</span>
+            Doctor Haus
+          </p>
           <p class="userCard-body-item">
             <span class="b">Country, city:</span>
             Belarus, Minsk
@@ -79,40 +84,140 @@ const editModal = ref({
       </div>
     </div>
 
-    <div name="Settings">
-      <vue-final-modal
-        v-model="editModal.show"
-        @click-outside="editModal.show = false"
-      >
-      <div class="Modal-header">
+    <vue-final-modal
+      v-model="editModal.show"
+      @click-outside="editModal.show = false"
+    >
+    <div class="Settings content">
+      <div class="Settings-header">
         <h2>
           Edit profile
         </h2>
         <button
           @click="editModal.show = false"
-          class="Modal-close"
+          class="Settings-close"
         >
           <img src="../../assets/media/img/close.svg" alt="X">
         </button>
       </div>
-        <div class="Modal-body">
+      <div class="Settings-body">
         <div class="Settings-item">
-          <label for="name" class="Settings-item-title">
-            Name and Surname
-          </label>
-          <input type="text" placeholder="Ivan Fedorov" class="Settings-item-input" id="name" name="name">
+          <div class="Settings-item-main">
+            <label for="name" class="Settings-item-title">
+              Name and Surname
+            </label>
+            <input type="text" value="Ivan Fedorov" class="Settings-item-input" id="name" name="name">
+          </div>
           <p class="Settings-item-publish">
             <label for="namePublish">
               Publish
             </label>
-            <input type="radio" name="namePublish" id="namePublish" value="false">
-            <input type="radio" name="namePublish" id="namePublishTrue" value="true">
+            <input type="checkbox" name="namePublish" id="namePublish" class="Settings-item-publish-switcher">
           </p>
         </div>
-      </div>
-      </vue-final-modal>
+
+        <div class="Settings-item">
+          <div class="Settings-item-main">
+            <label for="nickname" class="Settings-item-title">
+              Pseudonym / Nickname 
+            </label>
+            <input type="text" value="Ivan Fedorov" class="Settings-item-input" id="nickname" name="nickname">
+          </div>
+          <p class="Settings-item-publish">
+            <label for="nicknamePublish">
+              Publish
+            </label>
+            <input type="checkbox" name="nicknamePublish" id="nicknamePublish" class="Settings-item-publish-switcher">
+          </p>
+        </div>
+
+        <div class="Settings-item">
+          <div class="Settings-item-main">
+            <label for="birth" class="Settings-item-title">
+              Year of birth
+            </label>
+            <input type="text" value="1989" class="Settings-item-input" id="birth" name="birth">
+          </div>
+          <p class="Settings-item-publish">
+            <label for="birthPublish">
+              Publish
+            </label>
+            <input type="checkbox" name="birthPublish" id="birthPublish" class="Settings-item-publish-switcher">
+          </p>
+        </div>
+
+        <div class="Settings-item">
+          <div class="Settings-item-main">
+            <label for="place" class="Settings-item-title">
+              Country / City
+            </label>
+            <input type="text" value="Poland, Warsaw" class="Settings-item-input" id="place" name="place">
+          </div>
+          <p class="Settings-item-publish">
+            <label for="placePublish">
+              Publish
+            </label>
+            <input type="checkbox" name="placePublish" id="placePublish" class="Settings-item-publish-switcher">
+          </p>
+        </div>
+
+        <div class="Settings-item">
+          <div class="Settings-item-main">
+            <label for="instagramNick" class="Settings-item-title">
+              @instagram
+            </label>
+            <input type="text" value="@Ivan_Fedorov" class="Settings-item-input" id="instagramNick" name="instagramNick">
+          </div>
+          <p class="Settings-item-publish">
+            <label for="instagramNickPublish">
+              Publish
+            </label>
+            <input type="checkbox" name="instagramNickPublish" id="instagramNickPublish" class="Settings-item-publish-switcher">
+          </p>
+        </div>
+
+        <div class="Settings-item">
+          <div class="Settings-item-main">
+            <label for="facebookNick" class="Settings-item-title">
+              @facebook
+            </label>
+            <input type="text" value="@IvanFedorov" class="Settings-item-input" id="facebookNick" name="facebookNick">
+          </div>
+          <p class="Settings-item-publish">
+            <label for="facebookNickPublish">
+              Publish
+            </label>
+            <input type="checkbox" name="facebookNickPublish" id="facebookNickPublish" class="Settings-item-publish-switcher">
+          </p>
+        </div>
+
+        <div class="Settings-item Settings-item_reason">
+          <div class="Settings-item-main">
+            <label for="reason" class="Settings-item-title">
+              Why did you decide to participate?
+            </label>
+            <textarea class="Settings-item-input" id="reason" name="reason">Из Беларуси, была репрессирована, в данный момент в эмиграции. Семья осталась дома, поскорее хочу вернуться. Для этого необходимо завершить начатое дело - свершение режима диктатуры, прекращение насилия, восстановление законности  в Беларуси. Единственное доступное оружие беларусов в эмиграции против режима  - придание огласке преступлений и привлечение</textarea>
+          </div>
+          <p class="Settings-item-publish">
+            <label for="reasonPublish">
+              Publish
+            </label>
+            <input type="checkbox" name="reasonPublish" id="reasonPublish" class="Settings-item-publish-switcher">
+          </p>
+        </div>
+
+        <div class="Settings-item Settings-item_buttons">
+          <div class="Settings-item-main">
+            <button class="button">Cancel</button>
+            <button class="button bg_black">Save</button>
+          </div>
+        </div>
+      </div>      
     </div>
+    </vue-final-modal>
+
   </main>
 </template>
 
 <style src="./UserCabinet.scss" lang="scss" scoped></style>
+<style src="./Settings.scss" lang="scss" scoped></style>
