ade782fa174d4dd195f0a649c7f0c20d0131eccd
ivalynx <inkybasik@gmail.com>
2023-01-13 21:08:54 +0200

    .Lang -> Lang Burger-menu-item

--- components/general/MainMenu/MainMenu.vue
+++ components/general/MainMenu/MainMenu.vue
@@ -66,8 +66,8 @@
           >
             {{ $t('participateBtnMsg') }}
           </a>
-          <details class="Lang">
-            <summary class="Lang-button Burger-menu-item">
+          <details class="Lang Burger-menu-item">
+            <summary class="Lang-button">
               EN<svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg" class="arrow" alt="Arrow">
                 <path d="M8.85522 0.878982L8.14812 0.171875L4.36684 3.95315L0.707107 0.293424L0 1.00053L3.65973 4.66026L3.65381 4.66617L4.36092 5.37328L8.85522 0.878982Z" fill="#4F4F4F"/>
                 </svg>
