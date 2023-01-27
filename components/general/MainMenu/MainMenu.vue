5af808c45bac639a17d0b4a1d93b6d2e34c96458
ivalynx <inkybasik@gmail.com>
2023-01-27 18:03:15 +0200

    Burder to js; User to js; Lang to js

--- components/general/MainMenu/MainMenu.vue
+++ components/general/MainMenu/MainMenu.vue
@@ -4,10 +4,70 @@
   const availableLocales = computed(() => {
     return (locales.value).filter(i => i.code !== locale.value)
   })
+
+  // const burgerButton = document.querySelector('.Burger-button');
+
+  // function burgerButtonIsClikable() {   
+  //   const burgerButtonItems = Array.from(document.querySelectorAll('.Burger-button__item'));
+  //   const burgerMenu = document.querySelector('.Burger-menu');
+  //   const burgerMenuItems = Array.from(document.querySelectorAll('.Burger-menu__item'));
+  //   const body = document.querySelector('body');
+  //   const Content = document.querySelector('.Content');
+  //   const Footer = document.querySelector('.Footer');
+  //   burgerButton.onclick = function(){
+  //     burgerButton.classList.toggle('Burger-button_close');
+  //     burgerButtonItems.forEach(element => {
+  //       element.classList.toggle('Burger-button_close__item');
+  //     });
+  //     burgerMenu.classList.toggle('Burger-menu_open');
+  //     burgerMenuItems.forEach(element => {
+  //       element.classList.toggle('Burger-menu_open__item');
+  //     });
+  //     body.classList.toggle('showOnlyHeader');
+  //     if( burgerMenu.classList.contains('Burger-menu_open') ) {
+  //      Content.hidden = true;
+  //      Footer.hidden = true;
+  //      } else {    
+  //      Content.hidden = false;
+  //      Footer.hidden = false;
+  //      };
+  //     };
+  // }
+
+  // function checkWindowSize() {
+  //   if( window.innerWidth > 799 ) {
+  //     burgerButton.hidden = true;
+  //   } else {      
+  //     burgerButton.hidden = false;
+  //     burgerButtonIsClikable();
+  //   }
+  // }
+  // checkWindowSize();
+
+  // window.onresize = () => {
+  //   checkWindowSize();
+  // };
+
+  // const UserButton = document.querySelector('.User-button');
+  // const LangButton = document.querySelector('.LangButton');
+  // const UserSelect = document.querySelector('.UserSelect');
+  // const LangSelect = document.querySelector('.LangSelect');
+  // UserButton.onclick = function(){
+  //   UserSelect.hidden = false:true;
+  //   if( !LangSelect.hidden ) {
+  //     LangSelect.hidden = false;
+  //   }
+  // };
+  // LangButton.onclick = function(){
+  //   LangSelect.hidden = false:true;
+  //   if( !UserSelect.hidden ) {
+  //     UserSelect.hidden = false;
+  //   }
+  // };
+  
 </script>
 
 <template>
-  <input type="checkbox" id="Burger" class="Burger-checker visually-hidden">
   <header class="Header">
     <div class="content">
       <a href="/" class="logo-project">
@@ -15,40 +75,40 @@
           {{ $t('title') }}
         </span>
       </a>
-      <label for="Burger" class="Burger-button" role="button" aria-role="button">
-        <span class="Burger-button-item">—</span>
-        <span class="Burger-button-item">—</span>
-        <span class="Burger-button-item">—</span>
-      </label>
+      <button class="Burger-button" role="button" aria-role="button">
+        <span class="Burger-button__item">—</span>
+        <span class="Burger-button__item">—</span>
+        <span class="Burger-button__item">—</span>
+      </button>
       <nav class="Burger-menu">
         <div class="Burger-menu-group">
          <a
          href="/"
-           class="Burger-menu-item"
+           class="Burger-menu__item"
          >
           {{ $t('linkHome') }}
          </a>
          <nuxt-link
            to="Gallery"
-           class="Burger-menu-item"
+           class="Burger-menu__item"
           >
            {{ $t('linkGallery') }}
           </nuxt-link>
           <nuxt-link
             to="News"
-            class="Burger-menu-item"
+            class="Burger-menu__item"
           >
             {{ $t('linkNews') }}
           </nuxt-link>
           <nuxt-link
             to="AboutUs"
-            class="Burger-menu-item"
+            class="Burger-menu__item"
           >
             {{ $t('linkAboutUs') }}
           </nuxt-link>
 <!--          <nuxt-link -->
 <!--            to="FAQ"-->
-<!--            class="Burger-menu-item"-->
+<!--            class="Burger-menu__item"-->
 <!--          >-->
 <!--            {{ $t('linkFAQ') }}-->
 <!--          </nuxt-link>-->
@@ -56,26 +116,27 @@
         <div class="Burger-menu-group">
           <nuxt-link
             to="SignIn"
-            class="Burger-menu-item button bg_red"
+            class="Burger-menu__item button bg_red"
           >
             {{ $t('participateBtnMsg') }}
           </nuxt-link>
-          <details class="User Burger-menu-item">
-            <summary class="User-button">
+          <div class="User Burger-menu__item">
+            <button class="User-button">
+              <span class="UserButton-text">Profile</span>
               <img src="/assets/media/img/login.svg" alt="Profile icon" class="UserButton-icon">
               <span class="iconMessage">
                 <span class="iconMessage-text">You have unfinished work</span>
               </span>
-              <span class="UserButton-text">Profile</span>
-            </summary>
-            <div class="UserSelect">
-              <img src="../../assets/media/img/Avatar.svg" class="UserSelect-item" alt="Avatar">
-              <h2 class="UserSelect-item UserSelect-item_title">Alexander Ivanow</h2>
+            </button>
+            <div class="UserSelect" hidden>
+              <img src="../../../assets/media/img/Avatar.svg" class="UserSelect-item UserSelect-item_avatar" alt="Avatar">
+              <span class="UserSelect-item UserSelect-item_title">Alexander Ivanow</span>
               <nuxt-link
                 to="UserCabinet"
                 class="UserSelect-item UserSelect-item_link">
                 My embroideries
-                <span class="iconMessage">!
+                <span class="iconMessage">
+                  <span class="iconMessage-icon">!</span>
                   <span class="iconMessage-text">You have unfinished work</span>
                 </span>                
               </nuxt-link>
@@ -85,14 +146,14 @@
                 Sign Out
               </nuxt-link>
             </div>
-          </details>
-          <details class="Lang Burger-menu-item">
-            <summary class="Lang-button">
+          </div>
+          <div class="Lang Burger-menu__item">
+            <button class="LangButton">
               EN<svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg" class="arrow" alt="Arrow">
                 <path d="M8.85522 0.878982L8.14812 0.171875L4.36684 3.95315L0.707107 0.293424L0 1.00053L3.65973 4.66026L3.65381 4.66617L4.36092 5.37328L8.85522 0.878982Z" fill="#4F4F4F"/>
                 </svg>
-            </summary>
-            <div class="Lang-select">
+            </button>
+            <div class="LangSelect" hidden>
               <nuxt-link
                   v-for="locale in availableLocales"
                   :key="locale.code"
@@ -107,7 +168,7 @@
                 English
               </span>
             </div>
-          </details>
+          </div>
         </div>
       </nav>
     </div>
