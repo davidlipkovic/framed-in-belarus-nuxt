13323698900c7793fd0ad2fdbfa153f2c2a6589c
david_lipkovic <david.lipkovic@gmail.com>
2023-04-12 03:42:24 +0200

    Changes packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     app: {
@@ -33,6 +31,7 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/i18n',
         '@nuxt/image-edge',
+        '@pinia/nuxt',
         // '@nuxtjs/tailwindcss'
     ],
 
