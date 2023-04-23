c4fde0f72f49e371cb5b14c3569c261983a1a132
david_lipkovic <david.lipkovic@gmail.com>
2023-04-23 22:51:01 +0200

    Styles profile page

--- pages/Profile/index.vue
+++ pages/Profile/index.vue
@@ -5,7 +5,7 @@ definePageMeta({
   layout: "embroidery"
 })
 
-const editModal = ref(false)
+const displayEditModal = ref(false)
 </script>
 
 <template>
@@ -40,7 +40,7 @@ const editModal = ref(false)
         <div class="userProfile-buttons">
           <button 
             class="button" 
-            @click="editModal = true"
+            @click="displayEditModal = true"
           >
             Edit profile
           </button>
@@ -62,145 +62,56 @@ const editModal = ref(false)
     </div>
 
     <GeneralModal
-      :displayModal="editModal"
+      @closeModal="displayEditModal = false"
+      :displayModal="displayEditModal"
     >
-    <div class="Settings content">
-      <div class="Settings-header">
-        <h2>
-          Edit profile
-        </h2>
-      </div>
-      <div class="Settings-body">
-        <div class="Settings-item Settings-item-name">
-          <div class="">
+      <div class="Settings content">
+        <div class="Settings-header">
+          <h2>
+            Edit profile
+          </h2>
+        </div>
+        <div class="Settings-body">
+          <div class="Settings-item">
             <div class="Settings-item-main">
-              <label for="name" class="Settings-item-title">
-                Name
+              <label for="nickname" class="Settings-item-title">
+                Pseudonym / Nickname
               </label>
-              <input type="text" value="Tiffany" class="Settings-item-input" id="name" name="name">
+              <input type="text" value="" placeholder="Your pseudonym / nickname" class="Settings-item-input" id="nickname" name="nickname">
+            </div>
+          </div>
+
+          <div class="Settings-item Settings-item_reason">
+            <div class="Settings-item-main">
+              <label for="reason" class="Settings-item-title">
+                Why did you decide to participate?
+              </label>
+              <textarea class="Settings-item-input" id="reason" name="reason">
+              </textarea>
             </div>
             <p class="Settings-item-publish">
-              <label for="namePublish">
+              <label for="reasonPublish">
                 Publish
               </label>
-              <input type="checkbox" name="namePublish" id="namePublish" class="switcher">
+              <input type="checkbox" name="reasonPublish" id="reasonPublish" class="switcher">
             </p>
           </div>
-          <div>
+
+          <div class="Settings-item Settings-item_buttons">
             <div class="Settings-item-main">
-              <label for="name" class="Settings-item-title">
-                Surname
-              </label>
-              <input type="text" value="Chin" class="Settings-item-input" id="name" name="name">
+              <button 
+                class="button" 
+                @click="displayEditModal = false"
+              >
+                Cancel
+              </button>
+              <button class="button bg_black">
+                Save
+              </button>
             </div>
-            <p class="Settings-item-publish">
-              <label for="namePublish">
-                Publish
-              </label>
-              <input type="checkbox" name="namePublish" id="namePublish" class="switcher">
-            </p>
-          </div>
-        </div>
-
-        <div class="Settings-item">
-          <div class="Settings-item-main">
-            <label for="nickname" class="Settings-item-title">
-              Pseudonym / Nickname
-            </label>
-            <input type="text" value="" placeholder="Your pseudonym / nickname" class="Settings-item-input" id="nickname" name="nickname">
-          </div>
-          <p class="Settings-item-publish">
-            <label for="nicknamePublish">
-              Publish
-            </label>
-            <input type="checkbox" name="nicknamePublish" id="nicknamePublish" class="switcher">
-          </p>
-        </div>
-
-        <div class="Settings-item">
-          <div class="Settings-item-main">
-            <label for="birth" class="Settings-item-title">
-              Year of birth
-            </label>
-            <input type="number" value="" class="Settings-item-input" id="birth" name="birth">
-          </div>
-          <p class="Settings-item-publish">
-            <label for="birthPublish">
-              Publish
-            </label>
-            <input type="checkbox" name="birthPublish" id="birthPublish" class="switcher">
-          </p>
-        </div>
-
-        <div class="Settings-item">
-          <div class="Settings-item-main">
-            <label for="place" class="Settings-item-title">
-              Country / City
-            </label>
-            <input type="text" value="USA" class="Settings-item-input" id="place" name="place">
-          </div>
-          <p class="Settings-item-publish">
-            <label for="placePublish">
-              Publish
-            </label>
-            <input type="checkbox" name="placePublish" id="placePublish" class="switcher">
-          </p>
-        </div>
-
-        <div class="Settings-item">
-          <div class="Settings-item-main">
-            <label for="instagramNick" class="Settings-item-title">
-              Instagram
-            </label>
-            <input type="text" value="@tuffytiffchin" placeholder="Your @instagram nickname" class="Settings-item-input" id="instagramNick" name="instagramNick">
-          </div>
-          <p class="Settings-item-publish">
-            <label for="instagramNickPublish">
-              Publish
-            </label>
-            <input type="checkbox" name="instagramNickPublish" id="instagramNickPublish" class="switcher">
-          </p>
-        </div>
-
-        <div class="Settings-item">
-          <div class="Settings-item-main">
-            <label for="facebookNick" class="Settings-item-title">
-              Facebook
-            </label>
-            <input type="text" value="" placeholder="Your @facebook nickname" class="Settings-item-input" id="facebookNick" name="facebookNick">
-          </div>
-          <p class="Settings-item-publish">
-            <label for="facebookNickPublish">
-              Publish
-            </label>
-            <input type="checkbox" name="facebookNickPublish" id="facebookNickPublish" class="switcher">
-          </p>
-        </div>
-
-        <div class="Settings-item Settings-item_reason">
-          <div class="Settings-item-main">
-            <label for="reason" class="Settings-item-title">
-              Why did you decide to participate?
-            </label>
-            <textarea class="Settings-item-input" id="reason" name="reason">To support political prisoners that have been imprisoned for standing up for their rights. My family is from Hong Kong, which is currently going through a similar situation like Belarus. I am also an embroiderer myself, and by supporting this project, I feel that I'm supporting political prisoners all over the world. Speaking of political prisoners, I already have one in mind. I would like to focus on, and it is Maria Kalesnikava, who is a musician and politician from Belarus. I first knew about Maria from Rufina's IG stories regarding Maria's sentencing. Ever since I saw the short video when she was on trial, forming a heart sign while handcuffed, I became more curious about her and her story. Her story also reminded me of one of my favorite politicians in Hong Kong, where she is currently detained for "sedition" under the National Security Law.
-            </textarea>
-          </div>
-          <p class="Settings-item-publish">
-            <label for="reasonPublish">
-              Publish
-            </label>
-            <input type="checkbox" name="reasonPublish" id="reasonPublish" class="switcher">
-          </p>
-        </div>
-
-        <div class="Settings-item Settings-item_buttons">
-          <div class="Settings-item-main">
-            <button class="button" @click="editModal = false">Cancel</button>
-            <button class="button bg_black">Save</button>
           </div>
         </div>
       </div>
-    </div>
     </GeneralModal>
   </main>
 </template>
