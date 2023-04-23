0c3f67eff34c4ffbd6465356ea72d517d1dc97aa
david_lipkovic <david.lipkovic@gmail.com>
2023-04-23 22:49:47 +0200

    Creates modal component

---
+++ components/general/Modal/Modal.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { watchEffect } from 'vue'
+
+const props = defineProps({
+  displayModal: {
+    type: Boolean,
+    default: false
+  }
+})
+
+watchEffect(() => {
+  props.displayModal ? document.documentElement.classList.add('overflowHidden') : document.documentElement.classList.remove('overflowHidden')
+})
+</script>
+
+<template>
+  <div
+    class="modalWrapper flexRowCenter"
+    v-if="displayModal"
+  >
+    <div
+      class="modalBackgroundWrapper"
+      @click="$emit('closeModal')"
+    />
+    <div
+      class="modalContentWrapper flexRowCenter"
+    >
+      <button
+        @click="$emit('closeModal')"
+        class="closeButton"
+      >
+        <SvgClose />
+      </button>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style src="./Modal.scss" lang="scss"></style>
