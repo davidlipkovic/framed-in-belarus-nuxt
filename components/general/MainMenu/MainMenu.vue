77eb560c16bb4eb4cc2abd3af790bebf51e9f583
david_lipkovic <david.lipkovic@gmail.com>
2023-04-23 22:49:00 +0200

    Adds user to menu

--- components/general/MainMenu/MainMenu.vue
+++ components/general/MainMenu/MainMenu.vue
@@ -2,14 +2,21 @@
 import { onMounted, ref } from 'vue'
 
 const { locale, locales } = useI18n()
+
+const props = defineProps({
+  showUser: {
+    type: Boolean,
+    default: false
+  }
+})
+
 const switchLocalePath = useSwitchLocalePath()
 const availableLocales = computed(() => {
   return (locales.value).filter(i => i.code !== locale.value)
 })
 
-//   onMounted(() => {
-//     console.log(availableLocales)
-//   })
+onMounted(() => {
+})
 </script>
 
 <template>
@@ -56,34 +63,65 @@ const availableLocales = computed(() => {
   <!--            {{ $t('linkFAQ') }}-->
   <!--          </nuxt-link>-->
           </div>
-          <div class="Burger-menu-group Burger-menu-group-lang">
+          <div class="Burger-menu-group Burger-menu-group-lang flexRowCenter">
             <nuxt-link
+              v-if="!showUser"
               to="/SignIn"
               class="Burger-menu-item Login"
             >
               {{ $t('linkLogin') }}
             </nuxt-link>
             <nuxt-link
+              v-if="!showUser"
               to="/SignUp"
               class="Burger-menu-item button bg_red"
             >
               {{ $t('participateBtnMsg') }}
             </nuxt-link>
-           <details class="Lang">
-             <summary class="Lang-button Burger-menu-item">
-               ENG<img class="arrow" src="@/assets/media/img/arrow.svg" alt="Arrow">
-             </summary>
-             <div class="Lang-select">
-               <nuxt-link
-                 v-for="locale in availableLocales"
-                 :key="locale.code"
-                 :to="switchLocalePath(locale.code)"
-                 class="Lang-link"
-               >
-                 {{ locale.name }}
-               </nuxt-link>
-             </div>
-            </details>
+            <button
+              v-if="showUser"
+              class="Burger-menu-item askButton flexRowCenter"
+              @click="showQuestionModal = !showQuestionModal"
+            >
+              <img
+                src="../../../assets/media/img/help-circle.svg"
+              >
+              <span class="pattern-button-text">
+                Ask a question
+              </span>
+            </button>
+            <nuxt-link
+              v-if="showUser"
+              to="/Profile"
+              class="Burger-menu-item profileLink flexRowCenter"
+            >
+              <img
+                src="../../../assets/media/img/profileSymbolFramed.svg"
+              >
+              <span class="pattern-button-text">
+                Profile
+              </span>
+            </nuxt-link>
+            <div class="Lang">
+              <div class="Lang-button Burger-menu-item">
+                ENG
+                <img 
+                  class="arrow" 
+                  src="@/assets/media/img/arrow.svg" 
+                  alt="Arrow"
+                >
+              </div>
+              <div class="Lang-select">
+                <nuxt-link
+                  v-for="locale in availableLocales"
+                  :key="locale.code"
+                  :to="switchLocalePath(locale.code)"
+                  class="Lang-link"
+                >
+                  {{ locale.name }}
+                </nuxt-link>
+              </div>
+            </div>
           </div>
         </nav>
       </div>
