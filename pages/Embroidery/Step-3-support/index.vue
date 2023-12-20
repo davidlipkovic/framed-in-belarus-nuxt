2ddf888e960bacff9e56dd41cdf8157b2dd38c56
david_lipkovic <david.lipkovic@gmail.com>
2023-12-20 21:23:53 +0100

    Updates step3

--- pages/Embroidery/Step-3-support/index.vue
+++ pages/Embroidery/Step-3-support/index.vue
@@ -28,63 +28,96 @@ definePageMeta({
         :currentStep="3"
       />
       <section>
-        <input type="checkbox" class="visually-hidden" id="HeroHidden">
-        <RegistrationHeroBio class="textWidth"/>
+        <RegistrationHeroBio class="textWidth heroBioWrapper"/>
         <article>
-          <div class="textWidth">
-            <blockquote class="blockquote">
-              <p class="blockquote-content">
-                <span class="blockquote-quote_opened">“</span>
-                  {{ $t('embroidery.step3Page.quote.content') }}
-                <span class="blockquote-quote_closed">”</span>
-              </p>
-              <p class="blockquote-author">
-                {{ $t('embroidery.step3Page.quote.author') }}
-              </p>
-            </blockquote>
-            <p>
-              {{ $t('embroidery.step3Page.section1.paragraph1') }}
+          <blockquote class="blockquote">
+            <p class="blockquote-content">
+              “ {{ $t('embroidery.step3Page.quote.content') }} ”
             </p>
-          </div>
+            <p class="blockquote-author">
+              {{ $t('embroidery.step3Page.quote.author') }}
+            </p>
+          </blockquote>
+          <p class="section1">
+            {{ $t('embroidery.step3Page.section1.paragraph1') }}
+          </p>
           <dl class="SupportMethods textWidth">
             <dt class="SupportMethod-title">
               {{ $t('embroidery.step3Page.section2.title') }}
             </dt>
-            <dd class="SupportMethod-content">
-              {{ $t('embroidery.step3Page.section2.paragraph1.content1') }} <span class="likeA">{{ $t('embroidery.step3Page.section2.paragraph1.highlight1') }}</span> {{ $t('embroidery.step3Page.section2.paragraph1.content2') }} <span class="likeA">{{ $t('embroidery.step3Page.section2.paragraph1.highlight2') }}</span>.
+            <dd class="SupportMethod-content SupportMethod-socials">
+              {{ $t('embroidery.step3Page.section2.paragraph1.content1') }} 
+              <span>
+                {{ $t('embroidery.step3Page.section2.paragraph1.highlight1') }}
+              </span> {{ $t('embroidery.step3Page.section2.paragraph1.content2') }} 
+              <span>
+                {{ $t('embroidery.step3Page.section2.paragraph1.highlight2') }}
+              </span>
+              .
             </dd>
             <dt class="SupportMethod-title">
               {{ $t('embroidery.step3Page.section3.title') }}
             </dt>
             <dd class="SupportMethod-content">
               <p>
-                {{ $t('embroidery.step3Page.section3.paragraph1.content') }} <a href="https://prisoners.spring96.org/en" target="_blank">{{ $t('embroidery.step3Page.section3.paragraph1.highlight') }}</a>.
+                {{ $t('embroidery.step3Page.section3.paragraph1.content') }} 
+                <a 
+                  href="https://prisoners.spring96.org/en" 
+                  target="_blank"
+                >
+                  {{ $t('embroidery.step3Page.section3.paragraph1.highlight') }}
+                </a>
               </p>
               <br>
               <p>
+                {{ $t('embroidery.step3Page.section3.paragraph2.content1') }}
                 <a 
                   href="https://docs.google.com/forms/d/e/1FAIpQLSfhsgpGwDc9sskZ82LN_eKwwZBAX9U5kLARAFo2eR1UGw8Usg/viewform?usp=send_form" 
                   target="_blank"
                 >
                   {{ $t('embroidery.step3Page.section3.paragraph2.highlight') }}
                 </a>
-                  {{ $t('embroidery.step3Page.section3.paragraph2.content') }}
-                </p>
+                {{ $t('embroidery.step3Page.section3.paragraph2.content2') }}
+              </p>
               <br>
               <h2 class="title">
                 {{ $t('embroidery.step3Page.section3.paragraph3') }}
               </h2>
               <dl>
-                <dt><b>{{ $t('embroidery.step3Page.section3.paragraph4.highlight') }}</b></dt>
-                <dd>{{ $t('embroidery.step3Page.section3.paragraph4.content') }}</dd>
-                <dt><b>{{ $t('embroidery.step3Page.section3.paragraph5.highlight') }}</b></dt>
-                <dd>{{ $t('embroidery.step3Page.section3.paragraph5.content') }}</dd>
-                <dt><b>{{ $t('embroidery.step3Page.section3.paragraph6.highlight') }}</b></dt>
-                <dd>{{ $t('embroidery.step3Page.section3.paragraph6.content') }}</dd>
+                <dt>
+                  <b>
+                    {{ $t('embroidery.step3Page.section3.paragraph4.highlight') }}:
+                  </b>
+                </dt>
+                <dd>
+                  {{ $t('embroidery.step3Page.section3.paragraph4.content') }}
+                </dd>
+                <dt>
+                  <b>
+                    {{ $t('embroidery.step3Page.section3.paragraph5.highlight') }}:
+                  </b>
+                </dt>
+                <dd>
+                  {{ $t('embroidery.step3Page.section3.paragraph5.content') }}
+                </dd>
+                <dt>
+                  <b>
+                    {{ $t('embroidery.step3Page.section3.paragraph6.highlight') }}:
+                  </b>
+                </dt>
+                <dd>
+                  {{ $t('embroidery.step3Page.section3.paragraph6.content') }}
+                </dd>
               </dl>
             </dd>
             <dt class="SupportMethod-title">
-              {{ $t('embroidery.step3Page.section4.title.content') }} <a href="https://politzek.me/" target="_blank">{{ $t('embroidery.step3Page.section4.title.highlight') }}</a>
+              {{ $t('embroidery.step3Page.section4.title.content') }} 
+              <a 
+                href="https://politzek.me/" 
+                target="_blank"
+              >
+                {{ $t('embroidery.step3Page.section4.title.highlight') }}
+              </a>
             </dt>
             <dd class="SupportMethod-content">
               {{ $t('embroidery.step3Page.section4.paragraph1') }}
@@ -92,12 +125,40 @@ definePageMeta({
             <dt class="SupportMethod-title">
               {{ $t('embroidery.step3Page.section5.title') }}
             </dt>
-            <dd class="SupportMethod-content">
+            <dd class="SupportMethod-content SupportMethod-financial">
               <ul>
-                <li><a href="https://dissidentby.com/en/help" target="_blank">{{ $t('embroidery.step3Page.section5.paragraph1') }}</a></li>
-                <li><a href="https://bysol.org/en/" target="_blank">{{ $t('embroidery.step3Page.section5.paragraph2') }}</a></li>
-                <li><a href="https://Ineedhelpby.org" target="_blank">{{ $t('embroidery.step3Page.section5.paragraph3') }}</a></li>
-                <li><a href="https://en.stranafund.org" target="_blank">{{ $t('embroidery.step3Page.section5.paragraph4') }}</a></li>
+                <li>
+                  <a 
+                    href="https://dissidentby.com/en/help" 
+                    target="_blank"
+                  >
+                    {{ $t('embroidery.step3Page.section5.paragraph1') }}
+                  </a>
+                </li>
+                <li>
+                  <a 
+                    href="https://bysol.org/en/" 
+                    target="_blank"
+                  >
+                    {{ $t('embroidery.step3Page.section5.paragraph2') }}
+                  </a>
+                </li>
+                <li>
+                  <a 
+                    href="https://Ineedhelpby.org" 
+                    target="_blank"
+                  >
+                    {{ $t('embroidery.step3Page.section5.paragraph3') }}
+                  </a>
+                </li>
+                <li>
+                  <a 
+                    href="https://en.stranafund.org" 
+                    target="_blank"
+                  >
+                    {{ $t('embroidery.step3Page.section5.paragraph4') }}
+                  </a>
+                </li>
               </ul>
             </dd>
           </dl>
