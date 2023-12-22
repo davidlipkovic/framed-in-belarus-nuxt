019c08d6ca6aa1da3dc822bfa0b73185d900293e
david_lipkovic <david.lipkovic@gmail.com>
2023-12-22 22:38:50 +0100

    Updates step6 processing shipping

---
+++ pages/Embroidery/Step-6-processing-shipping/index.vue
@@ -0,0 +1,60 @@
+<script setup>
+definePageMeta({
+  layout: "embroidery"
+})
+</script>
+
+<template>
+  <main class="Content">
+    <Head>
+      <Title>#Framed in Belarus / Step 6 — Shipping</Title>
+      <Meta name="description" content="First step"/>
+    </Head>
+    <div class="Title">
+      <div class="content">
+        <h1>
+          <span class="subtitle">
+            {{ $t('embroidery.steps.step') }} 6
+          <span class="visually-hidden">— </span></span>
+          {{ $t('embroidery.step6Page.title') }}
+        </h1>
+        <GeneralGoBack
+          page="Profile"
+        />
+      </div>
+    </div>
+    <div class="content">
+      <RegistrationNavSteps
+        :currentStep="6"
+      />
+      <section class="thankYou Waiting limitedTextWidthLg">
+        <div class="thankYouContent">
+          <h2 class="title">
+            {{ $t('embroidery.step6Page.sectionSent.title') }}
+          </h2>
+          <br>
+          <p>
+            {{ $t('embroidery.step6Page.sectionSent.content1') }}
+          </p>
+          <p>
+            {{ $t('embroidery.step6Page.sectionSent.content2') }}
+          </p>
+          <p>
+            {{ $t('embroidery.step6Page.sectionSent.content3') }}
+          </p>
+        </div>
+        <div class="buttons flexColumnCenter">
+          <nuxt-link 
+            :to="localePath('/Profile')"
+            class="button"
+          >
+            {{ $t('embroidery.step6Page.sectionSent.forwardButton') }}
+          </nuxt-link>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<style src="../../../assets/style/steps.scss" lang="scss" scoped></style>
+<style src="./Step-6-processing-shipping.scss" lang="scss" scoped></style>
