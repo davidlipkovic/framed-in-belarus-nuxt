ab4e0c37ce9aa03216451808316e0f0f0994f450
david_lipkovic <david.lipkovic@gmail.com>
2024-01-13 20:55:52 +0100

    Creates NotificationModal

---
+++ components/general/NotificationModal/NotificationModal.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { watchEffect } from 'vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+    default: ''
+  },
+  message: {
+    type: String,
+    default: ''
+  },
+  link: {
+    type: String,
+    default: ''
+  },
+  linkMessage: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div 
+    class="notificationModal flexRowStart"
+    :class="{
+      'notificationModalQuestion': icon === 'question',
+      'notificationModalWarning': icon === 'warning',
+    }"
+  >
+    <SvgQustionCircle
+      v-if="icon === 'question'"
+    />
+    <SvgTriangleWarning
+      v-if="icon === 'warning'"
+    />
+    <span>
+      {{ message }}
+      <nuxt-link
+        v-if="link"
+        :to="localePath('/' + page)"
+      >
+        {{ linkMessage }}
+      </nuxt-link>
+    </span>
+  </div>
+</template>
+
+<style src="./NotificationModal.scss" lang="scss"></style>
