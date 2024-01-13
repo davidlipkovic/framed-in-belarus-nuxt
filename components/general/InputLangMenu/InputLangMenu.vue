59116360af19efa7eddbb40bbe7f8372d4c37928
david_lipkovic <david.lipkovic@gmail.com>
2024-01-13 20:55:41 +0100

    Creates inputLangMenu

---
+++ components/general/InputLangMenu/InputLangMenu.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+const { locales } = useI18n()
+
+const emit = defineEmits([
+  'chooseLanguage',
+])
+
+const props = defineProps({
+  isRegistration: {
+    type: Boolean,
+    default: false
+  },
+})
+
+const inputLangMenu = ref(null)
+const toggleinputLangMenu = ref(false)
+
+const currentLocale = ref(null)
+
+onClickOutside(inputLangMenu, () => {
+  toggleinputLangMenu.value = false
+})
+
+const handleChooseLanguage = (lang) => {
+  emit('chooseLanguage', lang)
+  currentLocale.value = lang
+  toggleinputLangMenu.value = false
+}
+</script>
+
+<template>
+  <div 
+    class="inputLangMenuWrapper"
+    ref="inputLangMenu"
+    @click="toggleinputLangMenu = !toggleinputLangMenu"
+  >
+    <span 
+      class="langButton flexRowCenter"
+      :class="{'langButtonPlaceholder' : !currentLocale}"
+    >
+      {{ currentLocale ? currentLocale : isRegistration ? $t('placeholders.chooseCommunicationLanguage') + '*' : $t('placeholders.chooseCommunicationLanguage')}}
+      <SvgArrowDown/>
+    </span>
+    <div 
+      v-if="toggleinputLangMenu"
+      class="inputLangMenu dropdownMenuWrapper flexColumnStart"
+    >
+      <button
+        v-for="locale in locales"
+        :key="locale.code"
+        class="langLink"
+        @click="handleChooseLanguage(locale.name)"
+      >
+        {{ locale.name }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style src="./InputLangMenu.scss" lang="scss"></style>
