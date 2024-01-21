1167ebc7ec8ebea0c0a064a2902db5704ae53717
david_lipkovic <david.lipkovic@gmail.com>
2024-01-21 19:17:47 +0100

    Updates dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    devtools: { enabled: false },
+
     app: {
         head: {
             htmlAttrs: {
@@ -33,7 +35,7 @@ export default defineNuxtConfig({
 
     modules: [
         '@nuxtjs/i18n',
-        '@nuxt/image-edge',
+        // '@nuxt/image-edge',
         '@pinia/nuxt',
         '@vueuse/nuxt',
         'floating-vue/nuxt',
