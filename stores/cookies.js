6ea77517a4b237438218622c5be6f9bf8f2edbc2
david_lipkovic <david.lipkovic@gmail.com>
2024-02-04 16:09:03 +0100

    WIP turn off cookies

--- stores/cookies.js
+++ stores/cookies.js
@@ -2,7 +2,7 @@ import { computed, reactive, ref } from "vue"
 import { defineStore } from "pinia"
 
 export const useCookiesStore = defineStore("cookies", () => {
-  const isChecked = ref(false)
+  const isChecked = ref(true)
 
   return {
     isChecked
