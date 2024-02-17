a0d7722176e4250d0cd7f0ce528ed95253941f9f
david_lipkovic <david.lipkovic@gmail.com>
2024-02-17 10:41:55 +0100

    Updates heroes store

--- stores/heroes.js
+++ stores/heroes.js
@@ -2,74 +2,25 @@ import { computed, reactive, ref } from "vue"
 import { defineStore } from "pinia"
 
 import tagsJSON from '../assets/json/tags.json'
-import heroesJSON from '../assets/json/heroes.json'
 
 export const useHeroesStore = defineStore("heroes", () => {
   const loading = ref(false)
   const originalHeroes = reactive([])
-  const heroesAlphabetically = computed(() => Array.isArray(originalHeroes.value) ? [...originalHeroes.value].sort((a, b) => a.name.localeCompare(b.name)) : [])
+  const availableHeroes = computed(() => Array.isArray(originalHeroes.value) ? [...originalHeroes.value].filter((hero) => hero.kit[0].status === 'Taken') : [])
+  const heroesAlphabetically = computed(() => Array.isArray(availableHeroes.value) ? [...availableHeroes.value].sort((a, b) => a.name.localeCompare(b.name)) : [])
   const heroesAlphabeticallyReversed = computed(() => Array.isArray(heroesAlphabetically.value) ? [...heroesAlphabetically.value].reverse() : [])
-  const heroesChronologically = computed(() => Array.isArray(originalHeroes.value) ? [...originalHeroes.value].sort((a, b) => a.arrestedProgrammatic - b.arrestedProgrammatic) : [])
+  const heroesChronologically = computed(() => Array.isArray(availableHeroes.value) ? [...availableHeroes.value].sort((a, b) => a.arrestedProgrammatic - b.arrestedProgrammatic) : [])
   const heroesChronologicallyReversed = computed(() => Array.isArray(heroesChronologically.value) ? [...heroesChronologically.value].reverse() : [])
   const tags = reactive([])
 
-  // const chosenHero = (filterType, filterValue) => {
-  //   return originalHeroes.value.filter(hero => hero[filterType] === filterValue)
-  // }
-
-  const setDate = (originalDate) => {
-    let msec = Date.parse(originalDate)
-    return new Date(msec)
-  }
-
-  const chosenHero = {
-    id: 215,
-    url: "https://prisoners.spring96.org/en/person/maryja-kalesnikava",
-    name: "Maryia Kalesnikava",
-    case: "Seizure of power",
-    status: "active",
-    gender: "female",
-    photo: "https://spring96.org/files/images/kalesnikava.jpg",
-    description: "member of the presidium of the Coordination Council, head of the campaign headquarters of former presidential nominee Viktar Babaryka, sentenced to 11 years in prison",
-    birthday: "1982-04-24",
-    arrested: "2020-09-07",
-    articles0: "Art. 361 of the Criminal Code — Calls for actions aimed at causing harm to the national security of the Republic of Belarus",
-    articles1: "Art. 357 of the Criminal Code — Conspiracy to seize power in an unconstitutional way",
-    articles2: "Art. 361-1 of the Criminal Code — Creation of an extremist formation, or participation in it",
-    prisonTitle: "Penal colony No. 4",
-    prisonAddress: "246035, Homieĺ, vulica Antoshkina 3",
-    declaration: "https://spring96.org/en/news/99460",
-    decision: "11 years",
-    penalty: "imprisonment in a general-security penal colony",
-    judge: "Siarhei Yepikhau",
-    verdictDate: "2021-09-06",
-    appealDate: "2021-12-24",
-  }
-
-  // const chosenHero = ref(null)
+  const chosenHero = ref(null)
 
   const setChosenHero = (id) => {
     chosenHero.value = originalHeroes.value.find(hero => hero.id === id)
-  }
 
-  const removeEmpty = (heroes) => {
-    const nameRegex = /^[A-Z][a-z]+(\s[A-Z][a-z]+)?$/;
-    return heroes.filter(hero => {
-      if ((hero.name && hero.name !== "" && hero.name !== "FALSE" && nameRegex.test(hero.name) &&
-      (hero.arrested && hero.arrested !== "" && hero.arrested !== "FALSE"))) {
-        return hero
-      }
-    })
-  }
-  
-  const fixHeroesDates = (heroes) => {
-    return heroes.map(hero => {
-      if (hero.appealDate && hero.appealDate !== '') hero.appealDateProgrammatic = setDate(hero.appealDate)
-      if (hero.arrested && hero.arrested !== '') hero.arrestedProgrammatic = setDate(hero.arrested)
-      if (hero.birthday && hero.birthday !== '') hero.birthdayProgrammatic = setDate(hero.birthday)
-      if (hero.verdictDate && hero.verdictDate !== '') hero.verdictDateProgrammatic = setDate(hero.verdictDate)
-      return hero
-    })
+    if (window.sessionStorage) {
+      window.sessionStorage.setItem('fibChosenHero', JSON.stringify(chosenHero.value))
+    }
   }
 
   const endpointUrl = 'https://d2wpukog48e17c.cloudfront.net'
@@ -78,14 +29,11 @@ export const useHeroesStore = defineStore("heroes", () => {
     try {
       const {data: responseData} = await useFetch(endpointUrl + '/api/prisoners')
       originalHeroes.value = responseData.value.result
-      console.log(responseData.value.result);
-      // originalHeroes.value = fixHeroesDates(removeEmpty(heroesJSON))
+      console.log(responseData.value.result)
     } catch (error) {
       console.error('Error fetching prisoners list data:', error)
     }
   }
-
-  // originalHeroes.value = fixHeroesDates(removeEmpty(heroesJSON))
 
   tags.value = tagsJSON
 
