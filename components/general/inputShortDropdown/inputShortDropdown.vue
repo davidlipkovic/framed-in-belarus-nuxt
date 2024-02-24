6e4c510be7e89b288e1260611ed5f296e6af974a
david_lipkovic <david.lipkovic@gmail.com>
2024-02-24 16:44:23 +0100

    Creates shortDropdown

---
+++ components/general/inputShortDropdown/inputShortDropdown.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+
+const emit = defineEmits([
+  'chooseOption',
+])
+
+const props = defineProps({
+  chosenOption: {
+    type: String,
+    default: null
+  },
+  options: {
+    type: Array,
+    default: null
+  },
+  placeholder: {
+    type: String,
+    default: null
+  },
+  isRegistration: {
+    type: Boolean,
+    default: false
+  },
+})
+
+const dropdown = ref(null)
+const toggleDropdown = ref(false)
+
+onClickOutside(dropdown, () => {
+  toggleDropdown.value = false
+})
+
+const handleChooseOption = (lang) => {
+  emit('chooseOption', lang)
+  toggleDropdown.value = false
+}
+</script>
+
+<template>
+  <div 
+    class="dropdown shortDropdown"
+    :class="[
+      toggleDropdown ? 'inputShortDropdownOpened' : 'inputShortDropdownClosed',
+      {'inputShortDropdownRegistration' : isRegistration}
+    ]"
+    ref="dropdown"
+  >
+    <span 
+      class="dropdownButton flexRowCenter"
+      :class="{'dropdownButtonPlaceholder' : !chosenOption}"
+      @click="toggleDropdown = !toggleDropdown"
+    >
+      {{ chosenOption ? chosenOption : placeholder }}
+      <SvgArrowDown/>
+    </span>
+    <ul 
+      v-if="toggleDropdown"
+      class="dropdownWrapper flexColumnStart"
+    >
+      <li
+        v-for="(option, i) in options"
+        :key="i"
+      >
+        <button @click="handleChooseOption(option)">
+          {{ option.name }}
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style src="./inputShortDropdown.scss" lang="scss"></style>
