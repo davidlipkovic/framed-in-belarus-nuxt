e635f7aad200d489d113cb78ecfcd2d2262371fc
david_lipkovic <david.lipkovic@gmail.com>
2024-03-23 18:41:50 +0100

    Creates NotReady page

---
+++ pages/NotReady/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+definePageMeta({
+  layout: "nopointer"
+})
+</script>
+
+<template>
+  <main class="Content flexColumnCenter">
+    <div class="content NotReady flexColumnCenter">
+      <SvgMistake/>
+      <h1 class="title">
+        {{ $t('notReadyPage.title') }}
+      </h1>
+      <p class="description">
+        {{ $t('notReadyPage.subTitle') }}
+      </p>
+    </div>
+  </main>
+</template>
+
+<style src="./NotReady.scss" lang="scss" scoped></style>
