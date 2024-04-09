acb7cb114ac76701a5ecac0640bad28b0e029893
david_lipkovic <david.lipkovic@gmail.com>
2024-04-09 21:16:35 +0200

    Updates newsBox

--- components/news/NewsBox/NewsBox.vue
+++ components/news/NewsBox/NewsBox.vue
@@ -1,34 +1,71 @@
 <script setup>
 import { computed, onMounted, ref } from 'vue'
+import { useCurrentLocale } from "@/composables/CurrentLocale"
+
+const { getCurrentLocaleStringValue } = useCurrentLocale()
 
 const props = defineProps({
-  news: {
+  article: {
     type: Object,
     default: null
   },
+})
+
+const description = computed(() => {
+  const d = getCurrentLocaleStringValue(props.article, 'description_')
+  return d?.slice(0, 230) + '...'
+})
+
+const title = computed(() => {
+  return getCurrentLocaleStringValue(props.article, 'title_')
 })
 </script>
 
 <template>
   <div class="newsBoxWrapper flexColumnStart">
-    <nuxt-link
-      :to="news.link"
+    <div class="categoryTag">
+      {{ $t('newsPage.categories.' + article.category) }}
+    </div>
+    <a
+      v-if="article.readMoreLink"
+      :href="article.readMoreLink"
+      target="_blank"
       class="newsImageWrapper"
     >
       <img 
-        src="../../../assets/media/img/news/holidays-wish/title.jpg"
+        :src="article.photos[0].thumbnails.large.url"
         class="img" 
-        alt="Holiday's wish" 
+        :alt="article.photos[0].filename"
+      />
+    </a>
+    <nuxt-link
+      v-else-if="!article.readMoreLink"
+      :to="localePath('/News/Article/' + article.id)"
+      class="newsImageWrapper"
+    >
+      <img 
+        :src="article.photos[0].thumbnails.large.url"
+        class="img" 
+        :alt="article.photos[0].filename"
       />
     </nuxt-link>
     <h2 class="title b1">
-      {{ news.title }}
+      {{ title }}
     </h2>
     <p>
-      {{ news.perex }}
+      {{ description }}
     </p>
+    <a
+      v-if="article.readMoreLink"
+      :href="article.readMoreLink"
+      target="_blank"
+      class="button"
+    >
+      {{ $t('buttons.readMore') }}
+    </a>
     <nuxt-link
-      :to="news.link"
+      v-else-if="!article.readMoreLink"
+      :to="localePath('/News/Article/' + article.id)"
       class="button"
     >
       {{ $t('buttons.readMore') }}
