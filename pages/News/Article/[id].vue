ee169302f97a13340271874cf4513f720f9c929a
david_lipkovic <david.lipkovic@gmail.com>
2024-04-09 21:12:46 +0200

    Adds Article page

---
+++ pages/News/Article/[id].vue
@@ -0,0 +1,121 @@
+<script setup>
+import { computed, onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import MarkdownIt from 'markdown-it'
+import { useNewsStore } from "@/stores/news"
+import { useCurrentLocale } from "@/composables/CurrentLocale"
+
+definePageMeta({
+  middleware: [
+    'auth-general',
+    'news',
+  ],
+})
+
+const route = useRoute()
+const newsStore = useNewsStore()
+const { getCurrentLocaleStringValue } = useCurrentLocale()
+
+const slides = [
+  {alt: "Ala Lapatka"},
+  {alt: "Siarhey Hatskevich"},
+  {alt: "Tatsiana Kaneuskaya"},
+  {alt: "Pyatro Marchanka"},
+  {alt: "Vladzmir Zmurauka"},
+  {alt: "Marina Kirilchyk"},
+  {alt: "Dmitry Kubarau"},
+  {alt: "Dmitriy Dubkou"},
+  {alt: "Viacheslav Rahanchuk"},
+  {alt: "Ales Pushkin"}
+]
+
+const article = computed(() => {
+  return newsStore.articles.value.find(article => article.id === route.params.id)
+})
+
+const description = computed(() => {
+  const md = new MarkdownIt()
+  return md.render(getCurrentLocaleStringValue(article.value, 'description_'))
+  // return getCurrentLocaleStringValue(article.value, 'description_')
+})
+
+const title = computed(() => {
+  return getCurrentLocaleStringValue(article.value, 'title_')
+})
+
+const currentGallerySlide = ref(0)
+const showGallerySwiper = ref(false)
+
+const handleGallerySwiper = (i) => {
+  showGallerySwiper.value = true
+  currentGallerySlide.value = i
+
+  console.log(showGallerySwiper, currentGallerySlide)
+}
+</script>
+
+<template>
+  <main class="Content">
+    <Head>
+      <Title>
+        #Framed in Belarus - {{ title }}
+      </Title>
+    </Head>
+    <div>
+      <div class="Title">
+        <div class="content">
+          <h1 class="content News">
+            {{ title }}
+          </h1>
+          <GeneralGoBack
+            page="News"
+          />
+        </div>
+      </div>
+      <article class="content newsArticleContent">
+        <p class="infoWrapper infoWrapperCalendar flexRowStart">
+          <SvgCalendar class="ExhibitionListItem-descript-icon descriptIcon"/>
+          <b class="b2">
+            {{ article.startDate }} - {{ article.endDate }}
+          </b>
+        </p>
+        <p class="infoWrapper infoWrapperPlace flexRowStart">
+          <SvgLocation class="ExhibitionListItem-descript-icon descriptIcon"/>
+          <b class="b2">
+            {{ article.place }}, {{ article.city }}, {{ article.country }}
+          </b>
+        </p>
+        <div 
+          class="content newsDetailWrapper"
+          v-html="description"
+        />
+        <div class="galleryWrapper">
+          <img
+            v-for="(photo, i) in article.photos"
+            :key="photo.alt"
+            :src="photo.thumbnails.large.url"
+            :alt="photo.filename"
+            @click="handleGallerySwiper(i)"
+          />
+        </div>
+        <NewsFullScreenSwiper
+          :initialSlide="currentGallerySlide"
+          :showSwiper="showGallerySwiper"
+          :slides="article.photos"
+          @closeSwiper="showGallerySwiper = false"
+        />
+
+        <!-- <p>
+          The list of exhibeted embroideries:<br>
+          <a href="https://lcca.lv/en/survival-kit/" target="_blank">Ivan Ivanov</a>, 
+          <a href="https://lcca.lv/en/survival-kit/" target="_blank">Ivan Ivanov</a>, 
+          <a href="https://lcca.lv/en/survival-kit/" target="_blank">Ivan Ivanov</a>, 
+          <a href="https://lcca.lv/en/survival-kit/" target="_blank">Ivan Ivanov</a>, 
+          <a href="https://lcca.lv/en/survival-kit/" target="_blank">Ivan Ivanov</a>
+        </p> -->
+      </article>
+    </div>
+  </main>
+</template>
+
+<style src="./Article.scss" lang="scss"></style>
