09b309eebdd2db028bc4ccddb48e04281aec2313
david_lipkovic <david.lipkovic@gmail.com>
2024-04-09 22:57:11 +0200

    fixes robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,6 @@ export default defineNuxtConfig({
                 { property: 'og:image', content: '/main.jpg' },
                 { name: 'description', content: 'Framed in Belarus' },
                 { name: 'format-detection', content: 'telephone=no' },
-                { name: 'robots', content: 'noindex' }
             ],
             link: [
                 { rel: 'apple-touch-icon', type: 'image/x-icon', href: '/fav.svg' },
