d23fbcdf73ebd6a9d8795c74e944937c0230a7ce
david_lipkovic <david.lipkovic@gmail.com>
2024-04-09 21:33:15 +0200

    Updates packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
                 { property: 'og:image', content: '/main.jpg' },
                 { name: 'description', content: 'Framed in Belarus' },
                 { name: 'format-detection', content: 'telephone=no' },
+                { name: 'robots', content: 'noindex' }
             ],
             link: [
                 { rel: 'apple-touch-icon', type: 'image/x-icon', href: '/fav.svg' },
@@ -33,6 +34,7 @@ export default defineNuxtConfig({
     ],
 
     modules: [
+        '@nuxt/content',
         '@nuxtjs/i18n',
         // '@nuxt/image-edge',
         '@pinia/nuxt',
