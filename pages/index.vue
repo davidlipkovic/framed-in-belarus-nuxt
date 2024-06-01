4b9771140b32f45e1cd4de85106f2fb017d4adf8
david_lipkovic <david.lipkovic@gmail.com>
2024-06-01 17:07:20 +0200

    Adds support btn

--- pages/index.vue
+++ pages/index.vue
@@ -43,12 +43,20 @@ const numberOfHeroes = computed(() => {
         <p class="subtitle">
           {{ $t('description') }}
         </p>
-        <a
-          href="https://forms.gle/SKCcvWGzRkQxx2fH9"
-          class="button bg_red"
-        >
-          {{ $t('links.participate') }}
-        </a>
+        <div class="flexRowStart">
+          <a
+            href="https://forms.gle/SKCcvWGzRkQxx2fH9"
+            class="button bg_red"
+          >
+            {{ $t('links.participate') }}
+          </a>
+          <a
+            href="https://donorbox.org/framedinbelarus"
+            class="supportButton button button_border"
+          >
+            {{ $t('links.supportUs') }}
+          </a>
+        </div>
       </div>
       <div class="swiperWrapper">
         <div class="bg_red"></div>
