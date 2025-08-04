0e4398060d1b71eaf50385350f17d3619eaa3673
david_lipkovic <david.lipkovic@gmail.com>
2025-08-04 17:47:25 +0200

    Implements dayjs for nuxt

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
+<script setup>
+import { watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { $dayjs } = useNuxtApp()
+const { locale } = useI18n()
+
+watch(locale, (newLocale) => {
+  if (newLocale === 'bl') {
+    $dayjs.locale('be')
+  } else {
+    $dayjs.locale(newLocale || 'en')
+  }
+}, { immediate: true })
+</script>
+
 <template>
   <div>
     <NuxtLayout>
