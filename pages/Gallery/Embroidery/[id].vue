b8fad5330bf7ab4b4679dfb1ac108fe5d55b4080
david_lipkovic <david.lipkovic@gmail.com>
2026-02-26 11:35:09 +0100

    Removes corrections modal

--- pages/Gallery/Embroidery/[id].vue
+++ pages/Gallery/Embroidery/[id].vue
@@ -32,7 +32,6 @@ const caseHeaderWrapper = ref(null)
 const toggleTextScrollToTop = ref(null)
 
 // Corections
-const displayCorrectionsModal = ref(false)
 const success = ref(false)
 const successPublish = ref(false)
 const publishStudioPhotosSignature = ref(null)
@@ -127,13 +126,6 @@ const slides = computed(() => {
   ])
 })
 
-const validCorrectionsData = computed(() => 
-  publishStudioPhotosSignature.value !== null && 
-  publishComments.value !== null && 
-  publishProcessPhotosPersonalData.value !== null && 
-  exhibitSignature.value !== null 
-)
-
 onMounted(() => {
   if (!galleryStore.currentEmbroidery.prisoner.photo || galleryStore.currentEmbroidery.prisoner.photo === '' || galleryStore.currentEmbroidery.prisoner.photo === 'FALSE') {
     return
@@ -183,12 +175,13 @@ const publish = () => {
           </p>
         </div>
         <div class="btnsWrapper flexColumnCenter">
-          <button 
+          <nuxt-link 
+            :to="$localePath('/Corrections?id=' + galleryStore.currentEmbroidery.id)"
             class="button"
-            @click="displayCorrectionsModal = true"
+            target="_blank"
           >
             {{ $t('buttons.correctionsNeeded') }}
-          </button>
+          </nuxt-link >
           <button 
             class="button bg_black"
             @click="publish()"
@@ -436,221 +429,6 @@ const publish = () => {
         />
       </article>
     </div>
-    <GeneralInputModal
-      class="correctionsModal"
-      @closeModal="displayCorrectionsModal = false"
-      :displayModal="displayCorrectionsModal"
-    >
-      <div class="inputModalContentWrapper">
-        <div class="inputModalHeader flexRowStart">
-          <h2>
-            {{ $t('correctionsPage.title') }}
-          </h2>
-          <button
-            @click="displayCorrectionsModal = false"
-            class="closeButton"
-          >
-            <SvgClose/>
-          </button>
-        </div>
-        <div class="inputModalBody flexColumnStart">
-          <div class="inputModalRow flexRowStart">
-            <div class="inputModalItem inputModalItemFullWidth flexColumnStart">
-              <p class="description">
-                {{ $t('correctionsPage.description') }}
-              </p>
-            </div>
-          </div>
-          <div class="inputModalRow flexRowStart">
-            <div class="inputModalItem inputModalItemFullWidth flexColumnStart">
-              <div class="radioWrapper">
-                <p class="b1">
-                  {{ $t('correctionsPage.inputStudioPhotos.title') }}
-                </p>
-                <label 
-                  for="publishStudioPhotosSignature"
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="publishStudioPhotosSignature"
-                    id="publishStudioPhotosSignature"
-                    :value="true"
-                    v-model="publishStudioPhotosSignature"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputStudioPhotos.option1') }}
-                </label>
-                <label 
-                  for="publishStudioPhotosHiddenSignature" 
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="publishStudioPhotosHiddenSignature"
-                    id="publishStudioPhotosHiddenSignature"
-                    :value="false"
-                    v-model="publishStudioPhotosSignature"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputStudioPhotos.option2') }}
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="inputModalRow flexRowStart">
-            <div class="inputModalItem inputModalItemFullWidth flexColumnStart">
-              <div class="radioWrapper">
-                <p class="b1">
-                  {{ $t('correctionsPage.inputComments.title') }}
-                </p>
-                <label 
-                  for="publishCommentsPersonalData"
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="publishCommentsPersonalData"
-                    id="publishCommentsPersonalData"
-                    :value="'personalData'"
-                    v-model="publishComments"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputComments.option1') }}
-                </label>
-                <label 
-                  for="publishCommentsNoPersonalData" 
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="publishCommentsNoPersonalData"
-                    id="publishCommentsNoPersonalData"
-                    :value="'noPersonalData'"
-                    v-model="publishComments"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputComments.option2') }}
-                </label>
-                <label 
-                  for="noPublishComments" 
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="noPublishComments"
-                    id="noPublishComments"
-                    :value="false"
-                    v-model="publishComments"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputComments.option3') }}
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="inputModalRow flexRowStart">
-            <div class="inputModalItem inputModalItemFullWidth flexColumnStart">
-              <div class="radioWrapper">
-                <p class="b1">
-                  {{ $t('correctionsPage.inputProcessPhotos.title') }}
-                </p>
-                <label 
-                  for="publishProcessPhotosPersonalData"
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="publishProcessPhotosPersonalData"
-                    id="publishProcessPhotosPersonalData"
-                    :value="true"
-                    v-model="publishProcessPhotosPersonalData"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputProcessPhotos.option1') }}
-                </label>
-                <label 
-                  for="publishProcessPhotosNoPersonalData" 
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="publishProcessPhotosNoPersonalData"
-                    id="publishProcessPhotosNoPersonalData"
-                    :value="false"
-                    v-model="publishProcessPhotosPersonalData"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputProcessPhotos.option2') }}
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="inputModalRow flexRowStart">
-            <div class="inputModalItem inputModalItemFullWidth flexColumnStart">
-              <div class="radioWrapper">
-                <p class="b1">
-                  {{ $t('correctionsPage.inputOriginalEmbroidery.title') }}
-                </p>
-                <label 
-                  for="exhibitSignature"
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="exhibitSignature"
-                    id="exhibitSignature"
-                    :value="true"
-                    v-model="exhibitSignature"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputOriginalEmbroidery.option1') }}
-                </label>
-                <label 
-                  for="exhibitNoSignature" 
-                  class="flexRowStart"
-                >
-                  <input
-                    type="radio"
-                    name="exhibitNoSignature"
-                    id="exhibitNoSignature"
-                    :value="false"
-                    v-model="exhibitSignature"
-                    required
-                  />
-                  {{ $t('correctionsPage.inputOriginalEmbroidery.option2') }}
-                  <SvgHelpCircleThick
-                    v-tooltip="$t('correctionsPage.inputOriginalEmbroidery.option2Tooltip')"
-                  />
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="inputModalRow flexRowStart">
-            <div class="inputModalItem inputModalItemFullWidth flexColumnStart">
-              <p class="additionalInfo">
-                {{ $t('correctionsPage.additionalInfo.content1') }}<a href="mailto:framedinbelarus@gmail.com">{{ $t('correctionsPage.additionalInfo.highlight') }}</a>{{ $t('correctionsPage.additionalInfo.content2') }}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="inputModalFooter buttons">
-          <button 
-            class="button" 
-            @click="displayCorrectionsModal = false"
-          >
-            {{ $t('buttons.cancel') }}
-          </button>
-          <button 
-            class="button"
-            :class="validCorrectionsData ? 'bg_black' : 'button_disabled'"
-            @click="submitCorrections()"
-          >
-            {{ $t('buttons.submit') }}
-          </button>
-        </div>
-      </div>
-    </GeneralInputModal>
   </main>
 </template>
 
