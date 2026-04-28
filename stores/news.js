9a377a31b1f10495c8c45e6140659caad3fc6ed4
david_lipkovic <david.lipkovic@gmail.com>
2026-04-28 18:29:07 +0200

    Updates articles sorting

--- stores/news.js
+++ stores/news.js
@@ -1,11 +1,40 @@
 import { ref } from "vue"
 import { defineStore } from "pinia"
+import { useI18n } from 'vue-i18n'
 
 export default defineStore("news", () => {
   const loading = ref(false)
   const articles = ref(null)
-  const articlesPublication = computed(() => articles.value.sort((a, b) => a.publicationDate - b.publicationDate))
-  const articlesPublicationReversed = computed(() => articlesChronologically.value.reverse())
+
+  const articlesPublication = computed(() => {
+    const { t } = useI18n()
+
+    const acc = articles.value.sort((a, b) => a.publicationDate - b.publicationDate)
+    const currentArticlesAcc = []
+    const passedArticlesAcc = []
+
+    acc.forEach(article => {
+      const currentDate = new Date()
+      const startDate = new Date(article.startDate)
+      const endDate = new Date(article.endDate)
+
+      if (currentDate < startDate) {
+        article.status = t('newsPage.statuses.upcoming')
+        currentArticlesAcc.push(article)
+      } else if (currentDate > endDate) {
+        article.status = t('newsPage.statuses.passed')
+        passedArticlesAcc.push(article)
+      } else {
+        article.status = t('newsPage.statuses.ongoing')
+        currentArticlesAcc.push(article)
+      }
+    })
+
+    return [
+      ...currentArticlesAcc,
+      ...passedArticlesAcc,
+    ]
+  })
 
   const endpointUrl = 'https://d2wpukog48e17c.cloudfront.net'
 
@@ -32,7 +61,6 @@ export default defineStore("news", () => {
   return {
     articles,
     articlesPublication,
-    articlesPublicationReversed,
     loading,
     getArticles,
   }
